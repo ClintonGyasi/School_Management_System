9d159d563cb5a260e85b6760dace61dd96b8f492
ClintonGyasi <gyasibannorclinton@gmail.com>
2024-08-20 21:19:15 +0000

    Continuation of homepage update

--- index.php
+++ index.php
@@ -6,8 +6,44 @@
 <title>Welcome to New Life School</title>
 <link rel="stylesheet"	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 <link rel="stylesheet"href="css/style.css">
+
+<link rel="icon" href="imgs/pic2.jpeg" type="image/png">
+
 </head>
 <body class="body-home">
+    <div class="black-fill">
+        <div class="container">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="Imgs/Final.png" width="50">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        </ul>
+  </div>
+</nav>
+        </div>
+
+    </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
